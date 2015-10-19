ccc710a83f0ecedec4fdb5f550258051c0d55d1a
Cloudox <Cloudox@cloudoxdemacbook-pro.local>
2015-10-19 14:07:57 +0800

    Initial Commit

---
+++ sqlHM/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  sqlHM
+//
+//  Created by Cloudox on 15/10/19.
+//  Copyright © 2015年 Cloudox. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ sqlHM/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  sqlHM
+//
+//  Created by Cloudox on 15/10/19.
+//  Copyright © 2015年 Cloudox. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
